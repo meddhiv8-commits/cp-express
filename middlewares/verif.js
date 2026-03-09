66cc1c34e9f945e50cd41c992ffc61c37cc8f361
med dhia <meddhiv8@gmail.com>
2026-03-09 14:22:38 +0100

    aiud

---
+++ middlewares/verif.js
@@ -0,0 +1,12 @@
+app.use(express.static("public img"));
+function verif(req, res, next) {
+  const dateCourante = new Date();
+  const jours = dateCourante.getDay();
+  const heures = dateCourante.getHours();
+  if (jours > 0 && joures < 6 && heures > 9 && heures < 17) {
+    next();
+  } else {
+    res.send("le site est disponible du lundi au vendredi de 9h à 17h");
+  }
+}
+module.exports = verif;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+//config
+app.set("view engine", "ejs");
+app.set("views", "./views");
+//middelware
+const verif = require("./middlewares/verif");
+app.use(verif);
+//Routes
+//-----------------home------------------------
+app.get("/", (req, res) => {
+res.render("home");
+});
+//-----------------Services--------------------
+app.get("/services", (req, res) => {
+res.render("services");
+});
+//----------------contactes--------------------
+app.get("/contact", (req, res) => {
+res.render("contact");
+});
+
+// ----------------fin du p-------------------
+
+const PORT = 3500;
+
+app.listen(PORT, (err) => {
+err
+    ? console.log(err)
+    : console.log(`the server is running on http://localhost:${PORT}`);
+});
